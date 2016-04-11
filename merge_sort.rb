4a5053752c888c7c146668c9a05b6a507f598c69
Fabricio Carrara <fabricio_x@hotmail.com>
2016-04-11 13:24:50 +0500

    Finish merge sort

---
+++ merge_sort.rb
@@ -0,0 +1,33 @@
+def merge_sort(list)
+  n = list.size / 2
+ 
+  if n > 0
+    left = merge_sort(list[0..n-1])
+    right = merge_sort(list[n..-1])
+    return merge(left,right)
+  else
+    return list
+  end
+end
+
+def merge(left,right)
+  result = []
+  
+  while !left.empty? && !right.empty?      
+    if left[0] <= right[0]
+      result << left.shift
+    else
+      result << right.shift
+    end
+  end
+
+  # Either left or right may have elements left so we consume them.
+  result.concat left unless left.empty?
+  result.concat right unless right.empty?
+
+  return result
+   
+end
+
+list = [9,8,7,6,5,4,3,2,1,0]
+puts merge_sort(list)
